03f873f08be784b99e5aa56d9eb2ed093036ef27
JFZenarosa <jaireh62@gmail.com>
2022-06-05 13:40:04 +0800

    Basic personal Info are now being written on the txt file, but some functions like writing on the text box has not yet been tried

--- Contact Tracing VB/Form1.Designer.vb
+++ Contact Tracing VB/Form1.Designer.vb
@@ -22,17 +22,422 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Title_label = New System.Windows.Forms.Label()
+        Me.Name_label = New System.Windows.Forms.Label()
+        Me.Home_label = New System.Windows.Forms.Label()
+        Me.Email_label = New System.Windows.Forms.Label()
+        Me.Contact_label = New System.Windows.Forms.Label()
+        Me.nametxt = New System.Windows.Forms.TextBox()
+        Me.hometxt = New System.Windows.Forms.TextBox()
+        Me.emailtxt = New System.Windows.Forms.TextBox()
+        Me.numtxt = New System.Windows.Forms.TextBox()
+        Me.Date_label = New System.Windows.Forms.Label()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.Time_label = New System.Windows.Forms.Label()
+        Me.timer = New System.Windows.Forms.Label()
+        Me.tickticktime = New System.Windows.Forms.Timer(Me.components)
+        Me.label7 = New System.Windows.Forms.Label()
+        Me.no5 = New System.Windows.Forms.CheckBox()
+        Me.yes5 = New System.Windows.Forms.CheckBox()
+        Me.no4 = New System.Windows.Forms.CheckBox()
+        Me.yes4 = New System.Windows.Forms.CheckBox()
+        Me.no3 = New System.Windows.Forms.CheckBox()
+        Me.yes3 = New System.Windows.Forms.CheckBox()
+        Me.no2 = New System.Windows.Forms.CheckBox()
+        Me.yes2 = New System.Windows.Forms.CheckBox()
+        Me.no1 = New System.Windows.Forms.CheckBox()
+        Me.yes1 = New System.Windows.Forms.CheckBox()
+        Me.label12 = New System.Windows.Forms.Label()
+        Me.label11 = New System.Windows.Forms.Label()
+        Me.label10 = New System.Windows.Forms.Label()
+        Me.label9 = New System.Windows.Forms.Label()
+        Me.label8 = New System.Windows.Forms.Label()
+        Me.button2 = New System.Windows.Forms.Button()
+        Me.button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'Title_label
+        '
+        Me.Title_label.AutoSize = True
+        Me.Title_label.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Title_label.ForeColor = System.Drawing.Color.Maroon
+        Me.Title_label.Location = New System.Drawing.Point(28, 28)
+        Me.Title_label.Name = "Title_label"
+        Me.Title_label.Size = New System.Drawing.Size(591, 39)
+        Me.Title_label.TabIndex = 0
+        Me.Title_label.Text = "Mercury Drugs Contact Tracing Form"
+        '
+        'Name_label
+        '
+        Me.Name_label.AutoSize = True
+        Me.Name_label.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name_label.Location = New System.Drawing.Point(109, 92)
+        Me.Name_label.Name = "Name_label"
+        Me.Name_label.Size = New System.Drawing.Size(94, 20)
+        Me.Name_label.TabIndex = 1
+        Me.Name_label.Text = "Full Name:"
+        '
+        'Home_label
+        '
+        Me.Home_label.AutoSize = True
+        Me.Home_label.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Home_label.Location = New System.Drawing.Point(71, 123)
+        Me.Home_label.Name = "Home_label"
+        Me.Home_label.Size = New System.Drawing.Size(132, 20)
+        Me.Home_label.TabIndex = 2
+        Me.Home_label.Text = "Home Address:"
+        '
+        'Email_label
+        '
+        Me.Email_label.AutoSize = True
+        Me.Email_label.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Email_label.Location = New System.Drawing.Point(74, 156)
+        Me.Email_label.Name = "Email_label"
+        Me.Email_label.Size = New System.Drawing.Size(129, 20)
+        Me.Email_label.TabIndex = 3
+        Me.Email_label.Text = "Email Address:"
+        '
+        'Contact_label
+        '
+        Me.Contact_label.AutoSize = True
+        Me.Contact_label.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Contact_label.Location = New System.Drawing.Point(59, 188)
+        Me.Contact_label.Name = "Contact_label"
+        Me.Contact_label.Size = New System.Drawing.Size(144, 20)
+        Me.Contact_label.TabIndex = 4
+        Me.Contact_label.Text = "Contact Number:"
+        '
+        'nametxt
+        '
+        Me.nametxt.Location = New System.Drawing.Point(209, 92)
+        Me.nametxt.Name = "nametxt"
+        Me.nametxt.Size = New System.Drawing.Size(349, 20)
+        Me.nametxt.TabIndex = 5
+        '
+        'hometxt
+        '
+        Me.hometxt.Location = New System.Drawing.Point(209, 125)
+        Me.hometxt.Name = "hometxt"
+        Me.hometxt.Size = New System.Drawing.Size(349, 20)
+        Me.hometxt.TabIndex = 6
+        '
+        'emailtxt
+        '
+        Me.emailtxt.Location = New System.Drawing.Point(209, 158)
+        Me.emailtxt.Name = "emailtxt"
+        Me.emailtxt.Size = New System.Drawing.Size(349, 20)
+        Me.emailtxt.TabIndex = 7
+        '
+        'numtxt
+        '
+        Me.numtxt.Location = New System.Drawing.Point(209, 190)
+        Me.numtxt.Name = "numtxt"
+        Me.numtxt.Size = New System.Drawing.Size(349, 20)
+        Me.numtxt.TabIndex = 8
+        '
+        'Date_label
+        '
+        Me.Date_label.AutoSize = True
+        Me.Date_label.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Date_label.Location = New System.Drawing.Point(150, 219)
+        Me.Date_label.Name = "Date_label"
+        Me.Date_label.Size = New System.Drawing.Size(53, 20)
+        Me.Date_label.TabIndex = 9
+        Me.Date_label.Text = "Date:"
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(209, 219)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(187, 20)
+        Me.DateTimePicker1.TabIndex = 10
+        '
+        'Time_label
+        '
+        Me.Time_label.AutoSize = True
+        Me.Time_label.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Time_label.Location = New System.Drawing.Point(402, 219)
+        Me.Time_label.Name = "Time_label"
+        Me.Time_label.Size = New System.Drawing.Size(52, 20)
+        Me.Time_label.TabIndex = 11
+        Me.Time_label.Text = "Time:"
+        '
+        'timer
+        '
+        Me.timer.AutoSize = True
+        Me.timer.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.timer.Location = New System.Drawing.Point(460, 220)
+        Me.timer.Name = "timer"
+        Me.timer.Size = New System.Drawing.Size(0, 20)
+        Me.timer.TabIndex = 12
+        '
+        'tickticktime
+        '
+        Me.tickticktime.Enabled = True
+        Me.tickticktime.Interval = 1
+        '
+        'label7
+        '
+        Me.label7.AutoSize = True
+        Me.label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label7.Location = New System.Drawing.Point(186, 273)
+        Me.label7.Name = "label7"
+        Me.label7.Size = New System.Drawing.Size(273, 31)
+        Me.label7.TabIndex = 13
+        Me.label7.Text = "Covid Health Check"
+        '
+        'no5
+        '
+        Me.no5.AutoSize = True
+        Me.no5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.no5.Location = New System.Drawing.Point(572, 458)
+        Me.no5.Name = "no5"
+        Me.no5.Size = New System.Drawing.Size(48, 24)
+        Me.no5.TabIndex = 43
+        Me.no5.Text = "No"
+        Me.no5.UseVisualStyleBackColor = True
+        '
+        'yes5
+        '
+        Me.yes5.AutoSize = True
+        Me.yes5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.yes5.Location = New System.Drawing.Point(497, 458)
+        Me.yes5.Name = "yes5"
+        Me.yes5.Size = New System.Drawing.Size(56, 24)
+        Me.yes5.TabIndex = 42
+        Me.yes5.Text = "Yes"
+        Me.yes5.UseVisualStyleBackColor = True
+        '
+        'no4
+        '
+        Me.no4.AutoSize = True
+        Me.no4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.no4.Location = New System.Drawing.Point(572, 427)
+        Me.no4.Name = "no4"
+        Me.no4.Size = New System.Drawing.Size(48, 24)
+        Me.no4.TabIndex = 41
+        Me.no4.Text = "No"
+        Me.no4.UseVisualStyleBackColor = True
+        '
+        'yes4
+        '
+        Me.yes4.AutoSize = True
+        Me.yes4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.yes4.Location = New System.Drawing.Point(497, 427)
+        Me.yes4.Name = "yes4"
+        Me.yes4.Size = New System.Drawing.Size(56, 24)
+        Me.yes4.TabIndex = 40
+        Me.yes4.Text = "Yes"
+        Me.yes4.UseVisualStyleBackColor = True
+        '
+        'no3
+        '
+        Me.no3.AutoSize = True
+        Me.no3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.no3.Location = New System.Drawing.Point(572, 396)
+        Me.no3.Name = "no3"
+        Me.no3.Size = New System.Drawing.Size(48, 24)
+        Me.no3.TabIndex = 39
+        Me.no3.Text = "No"
+        Me.no3.UseVisualStyleBackColor = True
+        '
+        'yes3
+        '
+        Me.yes3.AutoSize = True
+        Me.yes3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.yes3.Location = New System.Drawing.Point(497, 396)
+        Me.yes3.Name = "yes3"
+        Me.yes3.Size = New System.Drawing.Size(56, 24)
+        Me.yes3.TabIndex = 38
+        Me.yes3.Text = "Yes"
+        Me.yes3.UseVisualStyleBackColor = True
+        '
+        'no2
+        '
+        Me.no2.AutoSize = True
+        Me.no2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.no2.Location = New System.Drawing.Point(572, 365)
+        Me.no2.Name = "no2"
+        Me.no2.Size = New System.Drawing.Size(48, 24)
+        Me.no2.TabIndex = 37
+        Me.no2.Text = "No"
+        Me.no2.UseVisualStyleBackColor = True
+        '
+        'yes2
+        '
+        Me.yes2.AutoSize = True
+        Me.yes2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.yes2.Location = New System.Drawing.Point(497, 365)
+        Me.yes2.Name = "yes2"
+        Me.yes2.Size = New System.Drawing.Size(56, 24)
+        Me.yes2.TabIndex = 36
+        Me.yes2.Text = "Yes"
+        Me.yes2.UseVisualStyleBackColor = True
+        '
+        'no1
+        '
+        Me.no1.AutoSize = True
+        Me.no1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.no1.Location = New System.Drawing.Point(572, 334)
+        Me.no1.Name = "no1"
+        Me.no1.Size = New System.Drawing.Size(48, 24)
+        Me.no1.TabIndex = 35
+        Me.no1.Text = "No"
+        Me.no1.UseVisualStyleBackColor = True
+        '
+        'yes1
+        '
+        Me.yes1.AutoSize = True
+        Me.yes1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.yes1.Location = New System.Drawing.Point(497, 334)
+        Me.yes1.Name = "yes1"
+        Me.yes1.Size = New System.Drawing.Size(56, 24)
+        Me.yes1.TabIndex = 34
+        Me.yes1.Text = "Yes"
+        Me.yes1.UseVisualStyleBackColor = True
+        '
+        'label12
+        '
+        Me.label12.AutoSize = True
+        Me.label12.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label12.Location = New System.Drawing.Point(154, 462)
+        Me.label12.Name = "label12"
+        Me.label12.Size = New System.Drawing.Size(315, 20)
+        Me.label12.TabIndex = 33
+        Me.label12.Text = "Have you been Diagnosed with COVID-19?"
+        '
+        'label11
+        '
+        Me.label11.AutoSize = True
+        Me.label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label11.Location = New System.Drawing.Point(317, 431)
+        Me.label11.Name = "label11"
+        Me.label11.Size = New System.Drawing.Size(152, 20)
+        Me.label11.TabIndex = 32
+        Me.label11.Text = "Are you vaccinated?"
+        '
+        'label10
+        '
+        Me.label10.AutoSize = True
+        Me.label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label10.Location = New System.Drawing.Point(101, 400)
+        Me.label10.Name = "label10"
+        Me.label10.Size = New System.Drawing.Size(368, 20)
+        Me.label10.TabIndex = 31
+        Me.label10.Text = "Are you feeling any sickness for the past few days?"
+        '
+        'label9
+        '
+        Me.label9.AutoSize = True
+        Me.label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label9.Location = New System.Drawing.Point(24, 369)
+        Me.label9.Name = "label9"
+        Me.label9.Size = New System.Drawing.Size(445, 20)
+        Me.label9.TabIndex = 30
+        Me.label9.Text = "Have you been in contact with a person with COVID-19 lately?"
+        '
+        'label8
+        '
+        Me.label8.AutoSize = True
+        Me.label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label8.Location = New System.Drawing.Point(148, 338)
+        Me.label8.Name = "label8"
+        Me.label8.Size = New System.Drawing.Size(321, 20)
+        Me.label8.TabIndex = 29
+        Me.label8.Text = "Have you been outside of the country lately?"
+        '
+        'button2
+        '
+        Me.button2.Location = New System.Drawing.Point(506, 531)
+        Me.button2.Name = "button2"
+        Me.button2.Size = New System.Drawing.Size(127, 35)
+        Me.button2.TabIndex = 45
+        Me.button2.Text = "Submit"
+        Me.button2.UseVisualStyleBackColor = True
+        '
+        'button1
+        '
+        Me.button1.Location = New System.Drawing.Point(413, 531)
+        Me.button1.Name = "button1"
+        Me.button1.Size = New System.Drawing.Size(87, 35)
+        Me.button1.TabIndex = 44
+        Me.button1.Text = "Clear"
+        Me.button1.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(560, 509)
+        Me.ClientSize = New System.Drawing.Size(645, 578)
+        Me.Controls.Add(Me.button2)
+        Me.Controls.Add(Me.button1)
+        Me.Controls.Add(Me.no5)
+        Me.Controls.Add(Me.yes5)
+        Me.Controls.Add(Me.no4)
+        Me.Controls.Add(Me.yes4)
+        Me.Controls.Add(Me.no3)
+        Me.Controls.Add(Me.yes3)
+        Me.Controls.Add(Me.no2)
+        Me.Controls.Add(Me.yes2)
+        Me.Controls.Add(Me.no1)
+        Me.Controls.Add(Me.yes1)
+        Me.Controls.Add(Me.label12)
+        Me.Controls.Add(Me.label11)
+        Me.Controls.Add(Me.label10)
+        Me.Controls.Add(Me.label9)
+        Me.Controls.Add(Me.label8)
+        Me.Controls.Add(Me.label7)
+        Me.Controls.Add(Me.timer)
+        Me.Controls.Add(Me.Time_label)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.Date_label)
+        Me.Controls.Add(Me.numtxt)
+        Me.Controls.Add(Me.emailtxt)
+        Me.Controls.Add(Me.hometxt)
+        Me.Controls.Add(Me.nametxt)
+        Me.Controls.Add(Me.Contact_label)
+        Me.Controls.Add(Me.Email_label)
+        Me.Controls.Add(Me.Home_label)
+        Me.Controls.Add(Me.Name_label)
+        Me.Controls.Add(Me.Title_label)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Contact Tracing Form"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents Title_label As Label
+    Friend WithEvents Name_label As Label
+    Friend WithEvents Home_label As Label
+    Friend WithEvents Email_label As Label
+    Friend WithEvents Contact_label As Label
+    Friend WithEvents nametxt As TextBox
+    Friend WithEvents hometxt As TextBox
+    Friend WithEvents emailtxt As TextBox
+    Friend WithEvents numtxt As TextBox
+    Friend WithEvents Date_label As Label
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents Time_label As Label
+    Friend WithEvents timer As Label
+    Friend WithEvents tickticktime As Timer
+    Private WithEvents label7 As Label
+    Private WithEvents no5 As CheckBox
+    Private WithEvents yes5 As CheckBox
+    Private WithEvents no4 As CheckBox
+    Private WithEvents yes4 As CheckBox
+    Private WithEvents no3 As CheckBox
+    Private WithEvents yes3 As CheckBox
+    Private WithEvents no2 As CheckBox
+    Private WithEvents yes2 As CheckBox
+    Private WithEvents no1 As CheckBox
+    Private WithEvents yes1 As CheckBox
+    Private WithEvents label12 As Label
+    Private WithEvents label11 As Label
+    Private WithEvents label10 As Label
+    Private WithEvents label9 As Label
+    Private WithEvents label8 As Label
+    Private WithEvents button2 As Button
+    Private WithEvents button1 As Button
 End Class
--- Contact Tracing VB/Form1.vb
+++ Contact Tracing VB/Form1.vb
@@ -1,3 +1,25 @@
 ﻿Public Class Form1
+    Private Sub button2_Click(sender As Object, e As EventArgs) Handles button2.Click
+        Dim form As System.IO.StreamWriter
+        form = My.Computer.FileSystem.OpenTextFileWriter("C:\Example\Contact Tracing Form.txt", True)
+        form.WriteLine("Mercury Drugs Contact Tracing Form")
+        form.WriteLine("")
+        form.WriteLine("Full Name: " + nametxt.Text)
+        form.WriteLine("Home Address: " + hometxt.Text)
+        form.WriteLine("Email Address: " + emailtxt.Text)
+        form.WriteLine("Contact Number: " + numtxt.Text)
+        form.WriteLine("Date: " + DateTimePicker1.Text)
+        form.WriteLine("Time: " + DateTime.Now.ToString("h:mm:ss tt"))
+        form.WriteLine("")
+        form.WriteLine("COVID-19 Health Check")
+        form.WriteLine("")
+        form.Close()
+    End Sub
+
+    Private Sub time(sender As Object, e As EventArgs)
+
+        timer.Text = DateTime.Now.ToLongTimeString()
+
+    End Sub
 
 End Class
